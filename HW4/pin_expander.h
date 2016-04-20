f22601b796e6ce2b933df7b23e75c1e87a154c81
Alexey Revinski <alexeyrevinski2017@u.northwestern.edu>
2016-04-20 16:23:26 -0500

    Expander initiation function possibly complete

--- HW4/pin_expander.h
+++ HW4/pin_expander.h
@@ -6,14 +6,14 @@
 // MCP23008 ADDRESSES AND VALUES
 #define IODIR   (char) 0x00
 #define GPIO    (char) 0x09
-#define G0      (char) 1<<0
-#define G1      (char) 1<<1
-#define G2      (char) 1<<2
-#define G3      (char) 1<<3
-#define G4      (char) 1<<4
-#define G5      (char) 1<<5
-#define G6      (char) 1<<6
-#define G7      (char) 1<<7
+#define GP0     (char) 1<<0
+#define GP1     (char) 1<<1
+#define GP2     (char) 1<<2
+#define GP3     (char) 1<<3
+#define GP4     (char) 1<<4
+#define GP5     (char) 1<<5
+#define GP6     (char) 1<<6
+#define GP7     (char) 1<<7
 
 void initExpander(char IO_dir);
 void setExpander(char pin, char level);
