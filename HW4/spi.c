d925d72c290d1b1d7dada7d8bea26b02a96a51b8
Alexey Revinski <alexeyrevinski2017@u.northwestern.edu>
2016-04-20 20:02:24 -0500

    Pins for SDO, SDI, and SS are assigned

--- HW4/spi.c
+++ HW4/spi.c
@@ -1,7 +1,9 @@
 #include "spi.h"
 void initSPI1()
 {
-    ;
+    SDI1Rbits.SDI1R     = 0b0100;//SDI1 - RPB8
+    RPB13Rbits.RPB13R   = 0b0011;//SDO1 - RPB13
+    RPB15Rbits.RPB15R   = 0b0011;//SS1  - RPB15
 }
 void setVoltage(char channel, char voltage)
 {
--- HW4/spi.h
+++ HW4/spi.h
@@ -1,5 +1,9 @@
 #ifndef DAC_H
 #define DAC_H
+
+#define VoutA (char) 0x00
+#define VoutB (char) 0x01
+
 void initSPI1();
 void setVoltage(char channel, char voltage);
 char SPI_IO(char write);
