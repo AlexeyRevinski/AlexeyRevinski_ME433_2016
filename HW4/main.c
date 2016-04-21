c7b46f7e5c26fcfd404295496d509efd7eedfd11
Alexey Revinski <alexeyrevinski2017@u.northwestern.edu>
2016-04-20 19:33:40 -0500

    setExpander function is complete (set/clear pins)

--- HW4/main.c
+++ HW4/main.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    char read_byte = 0x10;
+    char read_byte = 0x00;
     // PIC32 Setup
     __builtin_disable_interrupts();
     __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
@@ -13,12 +13,9 @@ int main()
     TRISAbits.TRISA4 = 0;       // RA4 is output
     TRISBbits.TRISB4 = 1;       // RB4 is input
     LATAbits.LATA4 = 0;			// LED is off
-    ANSELBbits.ANSB2 = 0;       // I2C2 analog off;
-    ANSELBbits.ANSB3 = 0;       // I2C2 analog off;
-    i2c_master_setup();
-    i2c_master_write(IODIR,0x00);
-    i2c_master_write(OLAT,0xFF);
-    LATAbits.LATA4 = 1;
+    initExpander(0x80);
+    i2c_master_write(OLAT,0b10101010);
+    setExpander(GP4,1);
     __builtin_enable_interrupts();
     
     while(1)
--- HW4/pin_expander.c
+++ HW4/pin_expander.c
@@ -2,12 +2,23 @@
 void initExpander(char IO_dir)
 {
     ANSELBbits.ANSB2 = 0;       // I2C2 analog off;
+    ANSELBbits.ANSB3 = 0;       // I2C2 analog off;
     i2c_master_setup();
-    //i2c_master_write(GPIO,IO_dir);
+    i2c_master_write(IODIR,IO_dir);
 }
 void setExpander(char pin, char level)
-{    
-    ;
+{
+    char read_byte,write_byte;
+    read_byte = i2c_master_read(OLAT);
+    if (level==0)
+    {
+        write_byte = read_byte&~(pin);
+    }
+    else if (level==1)
+    {
+        write_byte = read_byte|pin;
+    }
+    i2c_master_write(OLAT,write_byte);
 }
 char getExpander(char pin)
 {
