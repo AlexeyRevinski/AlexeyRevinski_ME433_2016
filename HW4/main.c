37c159e6314de0b8817b49f3363616cb7fe0a584
Alexey Revinski <alexeyrevinski2017@u.northwestern.edu>
2016-04-22 20:24:50 -0500

    Both waves have right frequency. Homework done.

--- HW4/main.c
+++ HW4/main.c
@@ -2,14 +2,23 @@
 #define  LEVELS     (int)       256
 #define  PI         (double)    3.14159265
 #define  I2C_FREQ   (int)       12000000
+#define  PIC_FREQ   (int)       48000000
 #define  A_FREQ     (int)       10
 #define  B_FREQ     (int)       5
-#define  UPDATER    (int)       I2C_FREQ/B_FREQ/LEVELS
+#define  A_UPDATER  (int)       PIC_FREQ/A_FREQ/LEVELS
+#define  B_UPDATER  (int)       PIC_FREQ/B_FREQ/LEVELS
 
 int main()
 {
-    int             Acount = 0, Bcount = 0;
-    unsigned char   Alevel = 0, Blevel = 0, half = 0;
+    int             Acount = 0, i = 0;
+    unsigned char   Alevel = 0, Blevel = 0;
+    unsigned char   sine[A_UPDATER];
+    for (;i<A_UPDATER;i++)
+    {
+        sine[i] = ((LEVELS-1)/2*sin(((double)(2*PI*Acount))/((double)LEVELS)*2))+(LEVELS/2);
+        Acount++;
+    }
+    Acount = 0;
     // PIC32 Setup
     __builtin_disable_interrupts();
     __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
@@ -19,6 +28,7 @@ int main()
     TRISAbits.TRISA4 = 0;       // RA4 is output
     TRISBbits.TRISB4 = 1;       // RB4 is input
     LATAbits.LATA4 = 0;			// LED is off
+    WDTCONbits.ON = 0;
     initExpander(GP7);          // Make GP7 (button) input, rest outputs
     initSPI1();
     __builtin_enable_interrupts();
@@ -26,13 +36,12 @@ int main()
     while(1)
     {
         _CP0_SET_COUNT(0);
-        half = !half;
         setVoltage(A,Alevel);
         setVoltage(B,Blevel);
-        Alevel = ((LEVELS-1)/2*sin(((double)(2*PI*Acount))/((double)UPDATER)))+(LEVELS/2);
+        Alevel = sine[Acount];
         Acount++;
-        if (half){Blevel++;}
+        Blevel++;
         //setExpander(GP0,(char)((getExpander()&GP7)==GP7));  //Set LED on/off
-        while(_CP0_GET_COUNT()<UPDATER){;}
+        while(_CP0_GET_COUNT()<(B_UPDATER/2)){;}
     }   
 }
