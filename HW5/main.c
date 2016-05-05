168fb5e63fe7124be98ac5b469ded100d2a2c39b
Alexey Revinski <alexeyrevinski2017@u.northwestern.edu>
2016-05-05 00:56:29 -0500

    Gyro and Accelerometer values displayed
    The values are not fixed for the two's complement problem...

--- HW5/main.c
+++ HW5/main.c
@@ -4,6 +4,7 @@
 #include "utilities.h"
 #include <stdio.h>
 
+#define MARGIN 5
 
 unsigned short txtcolor = WHITE;
 unsigned short scrcolor = BLACK;
@@ -15,7 +16,7 @@ int main(void)
     char            whoami_check = 0;
     unsigned char   data[14];
     short           output[7];
-    int             oc1_new=0,oc2_new=0;
+    int             oc1_new=0,oc2_new=0,c;
     // Setup
     __builtin_disable_interrupts();
      __builtin_mtc0(_CP0_CONFIG,_CP0_CONFIG_SELECT,0xa4210583);
@@ -34,11 +35,20 @@ int main(void)
     setScrColor(BLACK);
     clearline(line);
     LCD_clearScreen(scrcolor);
+    LCD_drawString(MARGIN,5,"Gyro X: ");
+    LCD_drawString(MARGIN,20,"Gyro Y: ");
+    LCD_drawString(MARGIN,35,"Gyro Z: ");
+    LCD_drawString(MARGIN,50,"Accl X: ");
+    LCD_drawString(MARGIN,65,"Accl Y: ");
+    LCD_drawString(MARGIN,80,"Accl Z: ");
+    LCD_drawString(MARGIN,95,"Temp  : ");
+    
+    /*
     sprintf(line,"Hello World %d!",var);
     LCD_drawString(28,32,line);
     clearline(line);
     setTxtColor(RED);
-    LCD_drawString(5,100,"\"!\" outside screen!");
+    LCD_drawString(5,100,"\"!\" outside screen!");*/
     
     
     /*
@@ -53,8 +63,13 @@ int main(void)
         _CP0_SET_COUNT(0);
         i2c_master_read_all(IMU_ADDR,OUT_TEMP_L,14,data);
         char2short(data,output,14);
-        oc1_new = (PER2+1)/2+(output[4]/20);
-        oc2_new = (PER2+1)/2+(output[5]/20);
+        for(;c<7;c++)
+        {
+            clearline(line);
+            sprintf(line,"%d",output[c]);
+            LCD_drawString(MARGIN+8*6,c*15+MARGIN,line);
+        }
+        c = 0;
         LATAbits.LATA4 = !LATAbits.LATA4;
         while(_CP0_GET_COUNT()<UPDATER){;}
     }        
